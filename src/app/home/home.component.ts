79d3c4c9d2a36df4d1ec17818de92f5778b88b4d
anitha0203 <anithab@visheshcountrycache.com>
2022-02-17 17:34:33 +0530

    AngulatTest

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { getLocaleDateFormat } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
@@ -7,29 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  zipCode:any
+  zipCode!:string
   data:any;
+  key: string = 'Name';
+  myItem!: string;
   zcode!: number;
-  url = 'http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=5a4b2d457ecbef9eb2a71e480b947604';
-  ccond!: number;      temp!: number;      temp_max!: number;      temp_min!: number;
+  url = 'http://api.openweathermap.org/data/2.5/weather?id=';
+  ccond!: string;      temp!: number;      temp_max!: number;      temp_min!: number;
   ngOnInit(): void {
       
   }
+  sun=false; snow=false; rain=false; clouds=false;
   constructor(private http: HttpClient) {}
 
     setZip(zipcode:any) {
         this.zcode = zipcode;
-        localStorage.setItem('ZipCode : ', zipcode);
-        this.getData();
-    }
+          localStorage.setItem('Code ', zipcode);
+          this.getData()
+        }
+
     getData() {
-        this.http.get(this.url).subscribe((res) => {
+        this.http.get(this.url + this.zcode+'&appid=5a4b2d457ecbef9eb2a71e480b947604').subscribe((res) => {
           this.data = res;
           this.ccond = this.data.weather[0].main;
           this.temp = this.data.main.temp;
           this.temp_max = this.data.main.temp_max;
           this.temp_min = this.data.main.temp_min;
-          console.log(this.ccond + '   ' + this.temp  + '   ' +this.temp_min + '   ' +this.temp_max );        
+          console.log(this.ccond + '   ' + this.temp  + '   ' +this.temp_min + '   ' +this.temp_max );
+          if(this.ccond=="Snow"){
+            this.snow=!this.snow
+          }
+          else if(this.ccond=="Rain"){
+            this.rain= !this.rain
+          }
+          else if(this.ccond=="Clouds"){
+            this.clouds=!this.clouds
+          }
+          else
+          this.sun = !this.sun
     });    
   }
 }
